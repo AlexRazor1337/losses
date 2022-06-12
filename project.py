53cd81048fff250a92adbd1145981cc9f983ece9
AlexRazor1337 <alexrazor1337@gmail.com>
2022-06-12 15:42:16 +0300

    Add image generation

--- project.py
+++ project.py
@@ -1,6 +1,49 @@
-import requests, re
+import requests, re, json, sys
 from lxml import html
 from tabulate import tabulate
+
+import PIL.ImageEnhance as ImageEnhance
+import PIL.Image as Image
+import PIL.ImageDraw as ImageDraw
+import PIL.ImageFont as ImageFont
+
+
+def tint_image(img, factor):
+    enhancer = ImageEnhance.Brightness(img)
+    return enhancer.enhance(factor)
+
+
+def draw_text_on_image(data, date, background_path, font_path, font_size, header_font_size, header_text):
+    HEADER_TEXT = header_text % date
+
+    img = tint_image(Image.open(background_path), 0.4)
+    draw = ImageDraw.Draw(img)
+
+    font = ImageFont.truetype(font_path, font_size)
+    header_font = ImageFont.truetype(font_path, header_font_size)
+
+    center_x, center_y = img.size[0] / 2, img.size[1] / 2
+    start_y = center_y - ((len(data) + 1) * font_size) / 2
+
+    draw.text((center_x - draw.textsize(HEADER_TEXT, font=header_font)[0] / 2, start_y - 3 * header_font_size), HEADER_TEXT, font=header_font, align='center', fill='yellow')
+    # iterate over data and draw each line of text
+    for i, line in enumerate(data):
+
+        text_width = draw.textsize(line[0], font=font)[0]
+        draw.text((center_x - text_width - 10, start_y + i * font_size), line[0] + ':', font=font, align='right')
+
+        if ', ' in line[1]:
+            for j, sub_line in enumerate(line[1].split(', ')):
+                draw.text((center_x + 10, start_y + i * font_size + j * font_size), sub_line.split('(+')[0], font=font, align='left')
+                if '(+' in sub_line:
+                    draw.text((center_x + 10 + draw.textsize(sub_line.split('(+')[0], font=font)[0], start_y + i * font_size + j * font_size), '(+' + sub_line.split('(+')[1], font=font, align='left', fill='red')
+        else:
+            draw.text((center_x + 10, start_y + i * font_size), line[1].split('(+')[0], font=font, align='left')
+            if '(+' in line[1]:
+                draw.text((center_x + 10 + draw.textsize(line[1].split('(+')[0], font=font)[0], start_y + i * font_size), '(+' + line[1].split('(+')[-1], font=font, align='left', fill='red')
+
+
+    img.save('./result.jpg')
 
 
 def print_table(headers, data):
@@ -46,8 +89,21 @@ def main():
     URL = 'https://index.minfin.com.ua/ua/russian-invading/casualties/'
     data, date = load_data(URL)
     data = data_to_table(parse_data(data))
-    headers = ['Category', f'Amount of losses ({date})']
-    print_table(headers, data)
+
+    # check if there is and argument and it is true
+    if len(sys.argv) > 1 and sys.argv[1] == '-i':
+        try:
+            with open('./config.json', encoding='utf8') as config_file:
+                config = json.load(config_file)
+                draw_text_on_image(data, date, config.get('background_path', './resources/image.jpeg'), config.get('font_path', './resources/font.ttf'), config.get('font_size', 30), config.get('header_font_size', 46), config.get('header_text', 'Протягом 24.02-%s\n орієнтовані втрати противника склали:'))
+        except FileNotFoundError:
+            print('Config file not found, creating new.')
+            # create config.json file
+            with open('./config.json', 'w', encoding='utf8') as config_file:
+                json.dump({'background_path': './resources/image.jpeg', 'font_path': './resources/font.ttf', 'font_size': 30, 'header_font_size': 46, 'header_text': 'Протягом 24.02-%s\n орієнтовані втрати противника склали:'}, config_file, indent=4, ensure_ascii=False)
+
+    else:
+        print_table(['Category', f'Amount of losses ({date})'], data)
 
 
 if __name__ == "__main__":
